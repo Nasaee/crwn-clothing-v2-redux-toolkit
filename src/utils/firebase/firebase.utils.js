a007398ea2770774c89289124304e383f1186ac5
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2023-09-29 23:43:47 +0700

    finished ep.188 Immutable Middleware

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -20,13 +20,14 @@ import {
   getDocs,
 } from 'firebase/firestore';
 
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: 'AIzaSyC711aSUZ-hzq-bQEHA4Dd2mMIKqCSa5Fo',
+  authDomain: 'crwn-clothing-4cdde.firebaseapp.com',
+  projectId: 'crwn-clothing-4cdde',
+  storageBucket: 'crwn-clothing-4cdde.appspot.com',
+  messagingSenderId: '1033350272988',
+  appId: '1:1033350272988:web:c13e2216cf27ff0a5da119',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
